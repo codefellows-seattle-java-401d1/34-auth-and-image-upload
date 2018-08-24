1974200596e9259a5175f3b4b383c58aa2bdd64b
Amy Cohen <anbcohen@gmail.com>
2018-08-24 11:10:56 -0700

    adding firebase auth to the login page and checking to see if the user is null for setting the visibility of the loggedin or loggedout options

--- app/src/main/java/com/amycohen/lab34authimage/LoginActivity.java
+++ app/src/main/java/com/amycohen/lab34authimage/LoginActivity.java
@@ -2,10 +2,20 @@ package com.amycohen.lab34authimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.loggedInOptions) LinearLayout mLoggedInOptions;
+    @BindView(R.id.loggedOutOptions) LinearLayout mLoggedOutOptions;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void updateUI (FirebaseUser user) {
+        if (user == null) {
+            mLoggedInOptions.setVisibility(View.GONE);
+            mLoggedOutOptions.setVisibility(View.VISIBLE);
+        } else {
+            mLoggedInOptions.setVisibility(View.VISIBLE);
+            mLoggedOutOptions.setVisibility(View.GONE);
+        }
     }
 }
