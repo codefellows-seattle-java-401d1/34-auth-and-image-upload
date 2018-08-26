4b99225f3928a6ba27d8bd9819c7848dd9690d3b
brandon-fenty <brandonfenty@gmail.com>
2018-08-26 09:15:49 -0700

    adds model

--- app/src/main/java/com/droid/yutani/myinsta/model/Post.java
+++ app/src/main/java/com/droid/yutani/myinsta/model/Post.java
@@ -1,4 +1,13 @@
 package com.droid.yutani.myinsta.model;
 
 public class Post {
+    public String imgUrl;
+    public String author;
+    public String description;
+
+    public Post (String imgUrl, String author, String description) {
+        this.imgUrl = imgUrl;
+        this.author = author;
+        this.description = description;
+    }
 }
