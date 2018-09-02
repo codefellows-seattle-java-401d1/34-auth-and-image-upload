ce45855726ec6549e862e928ce93dfcd332ab375
Suzanne Richman <suzanne@favoredfortune.com>
2018-09-01 19:50:21 -0700

    screenshots of all user scenarios for app, plus of actual Firebase Storage and DB, uploaded and now in project readme.

--- app/src/main/java/com/android/sooz/catstagram/FeedActivity.java
+++ app/src/main/java/com/android/sooz/catstagram/FeedActivity.java
@@ -7,21 +7,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
